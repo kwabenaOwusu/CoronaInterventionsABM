f6f5e0102179fe88c5f424d61f23e137637923d7
kwabenaOwusu <kwabena@aims.edu.gh>
2020-05-12 20:24:56 +0000

    Add files via upload

---
+++ loop_modules.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+### loop through functions
+initialize()  
+observe()
+    
+for j in range(1,num_years):  
+    update_one_unit_time()
+    observe()
+
+os.system("ffmpeg -v quiet -r 5 -i day_%04d.png -vcodec mpeg4  -y -s:v 1920x1080 sim_movie.mp4") # convert files to a movie
